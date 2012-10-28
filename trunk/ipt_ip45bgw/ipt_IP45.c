4a9456b5ea56d7892e408af90a466c195aecbb77
tpoder@cis.vutbr.cz <tpoder@cis.vutbr.cz@ec4c0e80-25c3-5789-19ee-e1883ea6ec5a>
2012-10-28 11:15:05 +0000

    ip45bgw updated according the documentation

--- trunk/ipt_ip45bgw/ipt_IP45.c
+++ trunk/ipt_ip45bgw/ipt_IP45.c
@@ -9,10 +9,10 @@
 #include <net/ip.h>
 
 #include "ip45.h"
-#include "ipt_IP45.h"
+#include "ipt_ip45bgw.h"
 
 MODULE_AUTHOR("Tomas Podermanski <tpoder@cis.vutbr.cz>");
-MODULE_DESCRIPTION("Xtables: IP45 - IP45 point of attachment translation module");
+MODULE_DESCRIPTION("Xtables: IP45 - IP45 Border Gateway Module");
 MODULE_LICENSE("GPL");
 
 
@@ -42,7 +42,7 @@ MODULE_LICENSE("GPL");
     ((unsigned char *)&addr)[2], \
     ((unsigned char *)&addr)[3]
 
-static void ip45_log(
+static void ip45bgw_log(
 	char str[], 
 	struct ip45hdr *ip45h)
 {
@@ -53,14 +53,14 @@ static void ip45_log(
 
 }
 
-static unsigned int ip45_tg(
+static unsigned int ip45bgw_tg(
 	struct sk_buff *skb,
 	const struct xt_target_param *par)
 {
 	struct ip45hdr *ip45h = (struct ip45hdr *)ip_hdr(skb);
-	const struct ipt_ip45_info *info = par->targinfo;
-	u_int32_t inner, outer;
-	int shlen = (32 - info->inner_length) / 8;
+	const struct ipt_ip45bgw_info *info = par->targinfo;
+	u_int32_t downstream, upstream;
+	int shlen = (32 - info->downstream_len) / 8;
 	int log = IPT_IP45_OPT_LOG & info->ip45flags;
 
 
@@ -77,38 +77,38 @@ static unsigned int ip45_tg(
 
 
 	if ( log ) {
-		ip45_log("INPUT", ip45h);
+		ip45bgw_log("INPUT", ip45h);
 	}
 	
-	memcpy(&inner, &info->inner, sizeof(inner));
-	memcpy(&outer, &info->outer, sizeof(outer));
+	memcpy(&downstream, &info->downstream, sizeof(downstream));
+	memcpy(&upstream, &info->upstream, sizeof(upstream));
 
-	/* test whether the source address is part og inner prefix  -> update return path */
+	/* test whether the source address is part og downstream prefix  -> update return path */
 	/* shift address to right (32 - masklen) / 4 */
-	if ( (ip45h->saddr << (32 - info->inner_length) ) == (inner << (32 - info->inner_length)) ) {
+	if ( (ip45h->saddr << (32 - info->downstream_len) ) == (downstream << (32 - info->downstream_len)) ) {
 		u_int8_t *s45addr = (u_int8_t *)&ip45h->s45addr;
 		u_int32_t oldip = ip45h->saddr;
 		
 		ip45h->smark += shlen;
-		memcpy(s45addr + 12 - ip45h->smark , &outer, sizeof(outer));
-		ip45h->saddr = outer;
+		memcpy(s45addr + 12 - ip45h->smark , &upstream, sizeof(upstream));
+		ip45h->saddr = upstream;
 		csum_replace4(&ip45h->check, oldip, ip45h->saddr);
 	}
 
-	/* test whether the destination address is outer address 
+	/* test whether the destination address is upstream address 
   	*  -> the packet goes from the upstream (public) 
 	*     network to the donwstream (private) network
 	*  -> update destination addr by assebling from ip45 
 	*     part (on the mark position) and prefix defined by rule  
  	*/
-	if ( ip45h->daddr == outer ) {
+	if ( ip45h->daddr == upstream ) {
 		u_int8_t *d45addr = (u_int8_t *)&ip45h->d45addr;
 		u_int8_t *daddr = (u_int8_t *)&ip45h->daddr;
 		u_int32_t oldip = ip45h->daddr;
 
 		if (ip45h->dmark > 0)  {
 			// if fwdlen is set to 0 we have already processed all levels of IP45 attache points
-			memcpy(daddr, &inner, 4 - shlen);
+			memcpy(daddr, &downstream, 4 - shlen);
 			memcpy(daddr + 4 - shlen, d45addr + 16 - (int)ip45h->dmark, shlen);
 			ip45h->dmark -= shlen;
 			csum_replace4(&ip45h->check, oldip, ip45h->daddr);
@@ -116,45 +116,45 @@ static unsigned int ip45_tg(
 	}
 
 	if ( log ) {
-		ip45_log("OUTPUT", ip45h);
+		ip45bgw_log("OUTPUT", ip45h);
 	}
 
 	return XT_CONTINUE;
 }
 
-static bool ip45_tg_check(
+static bool ip45bgw_tg_check(
 	const struct xt_tgchk_param *par)
 {
-	const struct ipt_ip45_info *info = par->targinfo;
+	const struct ipt_ip45bgw_info *info = par->targinfo;
 
-	if ( (IPT_IP45_OPT_INNER & info->ip45flags) == 0 || (IPT_IP45_OPT_OUTER & info->ip45flags) == 0 ) {
-		printk("IP45: you must specify both --" IPT_IP45_INNER " and --" IPT_IP45_OUTER "\n");
+	if ( (IPT_IP45_OPT_DOWNSTREAM & info->ip45flags) == 0 || (IPT_IP45_OPT_UPSTREAM & info->ip45flags) == 0 ) {
+		printk("IP45: you must specify both --" IPT_IP45_DOWNSTREAM " and --" IPT_IP45_UPSTREAM "\n");
 		return false;
     }
 	return true;
 }
 
-static struct xt_target ip45_tg_reg __read_mostly = {
-	.name 		= "IP45",
-	.target 	= ip45_tg,
-	.checkentry	= ip45_tg_check,
+static struct xt_target ip45bgw_tg_reg __read_mostly = {
+	.name 		= "ip45bgw",
+	.target 	= ip45bgw_tg,
+	.checkentry	= ip45bgw_tg_check,
 	.destroy	= NULL,
 	.family		= NFPROTO_IPV4,
-	.targetsize	= sizeof(struct ipt_ip45_info),
+	.targetsize	= sizeof(struct ipt_ip45bgw_info),
 	.table		= "mangle",
 	.hooks		= (1 << NF_INET_POST_ROUTING) | (1 << NF_INET_PRE_ROUTING) | (1 << NF_INET_FORWARD),
 	.me 		= THIS_MODULE,
 };
 
-static int __init ip45_tg_init(void)
+static int __init ip45bgw_tg_init(void)
 {
-	return xt_register_target(&ip45_tg_reg);
+	return xt_register_target(&ip45bgw_tg_reg);
 }
 
-static void __exit ip45_tg_exit(void)
+static void __exit ip45bgw_tg_exit(void)
 {
-	xt_unregister_target(&ip45_tg_reg);
+	xt_unregister_target(&ip45bgw_tg_reg);
 }
 
-module_init(ip45_tg_init);
-module_exit(ip45_tg_exit);
+module_init(ip45bgw_tg_init);
+module_exit(ip45bgw_tg_exit);

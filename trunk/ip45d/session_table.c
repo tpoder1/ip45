c3a65789fc44e1b57905a53d7047de5c549ccb39
tpoder@cis.vutbr.cz <tpoder@cis.vutbr.cz@ec4c0e80-25c3-5789-19ee-e1883ea6ec5a>
2013-05-10 20:22:16 +0000

    - session table structure added

--- trunk/ip45d/session_table.c
+++ trunk/ip45d/session_table.c
@@ -5,23 +5,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdint.h>
-
-struct session_entry_t  {
-	uint64_t init_s45addr[2];
-	uint64_t init_d45addr[2];
-	uint64_t last_s45addr[2];
-	uint64_t last_d45addr[2];
-	uint8_t proto;
-	uint16_t sport;
-	uint16_t dport;
-	uint64_t sid;
-	struct session_entry_t *next;
-} sesion_entry; 
-
-struct session_table_t {
-	struct session_entry_t *head;
-	int items;
-} session_table;
+#include "session_table.h"
 
 /* init session table  */
 void session_table_init(struct session_table_t *table) {
@@ -45,13 +29,14 @@ struct session_entry_t *session_table_add(struct session_table_t *table, struct 
 	memcpy(new, entry, sizeof(struct session_entry_t));
 	new->next = table->head;
 	table->head = new;
+	table->items += 1;
 	
 	return new;
 }
 
 /* lookup entry in the session_table */
 /* returns pointer to foud recor or NULL if matching record does not exists */
-struct session_entry_t* session_table_lookup_sid(struct session_table_t *table, uint16_t sid) {
+struct session_entry_t* session_table_lookup_sid(struct session_table_t *table, uint64_t sid) {
 	
 	struct session_entry_t *tmp; 
 	tmp = table->head; 

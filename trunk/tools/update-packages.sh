0964de9a86642dc2419d4671c587f269c4014651
tpoder@cis.vutbr.cz <tpoder@cis.vutbr.cz@ec4c0e80-25c3-5789-19ee-e1883ea6ec5a>
2013-11-30 11:30:13 +0000

    - build script updated for windows

--- trunk/tools/update-packages.sh
+++ trunk/tools/update-packages.sh
@@ -3,6 +3,8 @@
 # script peridically check SVN and in there is change new 
 # packages in packages directory are rebuilt (not part of SVN) 
 
+VERSION=0.$(cat ../ID | grep Revision | cut -d" " -f2)
+
 cd ~
 
 # checkout svn 
@@ -19,20 +21,38 @@ fi
 
 cd ~/ip45
 
+
 # build ip45d package...
 echo "Building ip45d distribution package..."
 cd ip45d
 make dist 
 mv *.tar.gz ~/packages/source/ip45d
+cd  ~/packages/source/ip45d/
+ln -s ip45d-${VERSION}.tar.gz ip45d-latest.tar.gz
 
 cd ~/ip45
+
+
+# build ip45d package...
+echo "Building ip45d windows binaries..."
+cd ip45d
+make win 
+mkdir -p ~/packages/windows/${VERSION}/
+mv *.exe ~/packages/windows/${VERSION}/
+cd ~/packages/windows
+rm latest
+ln -s ${VERSION} latest
+
+cd ~/ip45
+
 
 # build iptables module package...
 echo "Building ipt_ip45bgw packages...."
 cd ipt_ip45bgw
 make dist 
 mv *.tar.gz ~/packages/source/ipt_ip45bgw
-
+cd  ~/packages/source/ipt_ip45bgw/
+ln -s ipt_ip45bgw-${VERSION}.tar.gz ipt_ip45bgw-latest.tar.gz
 cd ~/ip45
 
 
